fba9504de600bb1aa5476698d797e6fdbfbb1a2a
Tom Chambers <tom.chambers@gmail.com>
2014-09-10 14:40:40 +0100

    renitialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myinnertruth::Application.config.secret_token = '85424c0d0901b97a045e0629b62b467ceef1daba66ad9e881ff2804cd18c0cb09ea82294dc9f3d6b3f95d4fc58d5a60721c640d6f0ea679ca334e14aea6e1c38'
